2b427393ebd622760beb2f1df3a0d91b89b19c37
Josh Jore <jjore@cpan.org>
2012-02-12 22:51:54 -0800

    Syntax fix

--- eg/profile.sh
+++ eg/profile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for method in stor data fast clon json; do
+for method in data fast json stor clon; do
+  echo $method
   for i in 1 2 3 4 5 6 7 8 9 10; do
-    echo $method
-    /usr/bin/time -f '%U' perl profile.pl
+    /usr/bin/time -f '%U' perl profile.pl $method 100000
   done
 done
